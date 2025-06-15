ff7a73165fa5761436075e6b5ead9460ffd1f152
vivekrqwat <vivekrwaturfsam@gmail.com>
2025-06-15 13:14:31 +0530

    ch

--- slices/slices.go
+++ slices/slices.go
@@ -1,15 +1,13 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var nums []int
-	var nums = make([]int, 5)
+	// var nums = make([]int, 5)
 
 	// nums = append(nums, 1)
 	// nums := []int{}
-	fmt.Println(nums)
-	fmt.Println(nums[0:2])
-	fmt.Println("length", len(nums))
+	// fmt.Println(nums)
+	// fmt.Println(nums[0:2])
+	// fmt.Println("length", len(nums))
 
 }
